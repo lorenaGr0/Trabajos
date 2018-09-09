df4aa5414288f07f83561a781d5b2d5a4beb97aa
lorenaGr0 <lorenaantonia.chipahua@alumno.uttehuacan.edu.mx>
2018-09-09 11:57:45 -0500

    nuevo

--- HomeWork/ AreaRectangulo.java
+++ HomeWork/ AreaRectangulo.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
-public class AreaRectangurlo{
+public class AreaRectangulo{
   public static void main (String[]args){
     Scanner ingreso = new Scanner (System.in);
     int base, altura, area;
-    System.out.println("Area del rectangulo: ");
+    System.out.println("Area del rectangulo");
     System.out.println ("Ingresa la base:");
     base= ingreso.nextInt();
     System.out.println("ingresa la altura:");
-    altura = ingreso.nes¿xtInt();
+    altura = ingreso.nextInt();
     area = base * altura;
     System.out.println("El area es:" +  area);
   }
