e90daa50aa0cacbc0481365e89174a279d186919
lorenaGr0 <lorenaantonia.chipahua@alumno.uttehuacan.edu.mx>
2018-09-09 22:00:36 -0500

    nuevo

---
+++ HomeWork/ SueldoTrabajador.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ public class Sueldo{
+  public static void main (String[]args){
+  Scanner ingreso= new Scanner(System.in);
+  System.out.println("Horas trabajadas: ");
+  int HT, PH, SS;
+  HT = ingreso.nextInt();
+  System.out.println("Pago por horas: ");
+  PH= ingreso.nextInt();
+  SS=HT*PH;
+  Systemout.println("Sueldo semanal del trabajador: " + SS);
+}
+}
