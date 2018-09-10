550ebbaed3f0009d7e6b086155d1776ef0063720
lorenaGr0 <lorenaantonia.chipahua@alumno.uttehuacan.edu.mx>
2018-09-09 19:18:31 -0500

    nuevo

--- HomeWork/ AreaFig.java
+++ HomeWork/ AreaFig.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
- public class AreaFigura{
+ public class AreaFig{
   public static void main(String[]args){
     Scanner sc = new Scanner(System.in);
     System.out.println("Ingrese el valor de la base del triangulos: ")
