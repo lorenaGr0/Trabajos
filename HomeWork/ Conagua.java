fa108eaab74e07584ba26929a3288b5c6f886db2
lorenaGr0 <lorenaantonia.chipahua@alumno.uttehuacan.edu.mx>
2018-09-09 22:43:36 -0500

    nuevo

---
+++ HomeWork/ Conagua.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Conagua{
+  public static void main (String [] args){
+    Scanner input = new Scanner(System.in);
+    System.out.println("Altura de la alberca: ");
+    double A= input.nextDouble();
+    System.out.println("Largo de la alberca: ");
+    double L =input.nextDouble();
+    System.out.println("Ancho de la alberca: ")
+    double N = input.nextDouble();
+    System.out.println("ingrese el costo del metro cubico de agua: ");
+    double CM= input.nextDouble();
+    double V;
+    V=(A*L*N);
+    double PAG;
+    PAG= V*CM;
+    System.out.println("Pago total de los metros cubicos consumidos: " PAG);
+  }
+}
