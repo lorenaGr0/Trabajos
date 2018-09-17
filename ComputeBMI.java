673579118c1a65548a374d1cc4e900121a41992a
lorenaGr0 <lorenaantonia.chipahua@alumno.uttehuacan.edu.mx>
2018-09-17 17:02:51 -0500

    nuevo

--- ComputeBMI.java
+++ ComputeBMI.java
@@ -1,7 +1,7 @@
-import java.out.Scanner;
+import java.util.Scanner;
  public class ComputeBMI{
    public static void main(String[]args){
-     Scanner input = new Scanner (System.in);
+     Scanner input = new Scanner(System.in);
      final double KILOGRAMS_PER_POUND= 0.453592;
      final double METERS_PER_INCH =0.0254;
 
@@ -11,11 +11,25 @@ import java.out.Scanner;
      System.out.println("Enter height in inches");
      double height = input.nextDouble();
 
+
      double weightInKilogrms = weight * KILOGRAMS_PER_POUND;
      double heightInMeters = height * METERS_PER_INCH;
-     
 
-
+     double bmi =weightInKilogrms / (heightInMeters * heightInMeters);
+     System.out.println("your bmi is " + bmi);
+     //double bmi= weightInKilogrms / Math.pow(heightInMeters , 2);
+     if (bmi < 16 )
+     System.out.println( "your are seriously underweight");
+     else if (bmi < 18)
+      System.out.println( "your are underweight");
+      else if ( bmi <24)
+       System.out.println( "your are normal weight");
+       else if ( bmi < 29)
+        System.out.println( "your are overweight");
+        else if ( bmi < 35)
+         System.out.println( "your are seriously overweight");
+         else
+          System.out.println( "your are gravely overweightss");
 
    }
  }
